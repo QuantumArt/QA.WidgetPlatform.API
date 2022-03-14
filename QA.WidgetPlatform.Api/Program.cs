b2d024be639ed5684babade63a9cfe536784c188
Tigran Khachatrian <KhachatrianTS@quantumart.ru>
2022-03-14 12:06:24 +0300

    #167241 add qp invalidation

--- QA.WidgetPlatform.Api/Program.cs
+++ QA.WidgetPlatform.Api/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Builder;
+using QA.DotNetCore.Engine.CacheTags;
+using QA.DotNetCore.Engine.CacheTags.Configuration;
 using QA.WidgetPlatform.Api.Application.Middleware;
 using QA.WidgetPlatform.Api.Infrastructure;
 
@@ -8,6 +10,14 @@ builder.Services.ConfigureBaseServices(builder.Configuration);
 var app = builder.Build();
 
 app.UseMiddleware<StatusCodeExceptionHandlerMiddleware>();
+//мидлвара для инвалидации кештегов
+//необходимо, чтобы было подключено services.AddCacheTagServices
+app.UseCacheTagsInvalidation(trackers =>
+{
+    //регистрация одного или нескольких ICacheTagTracker
+    //QpContentCacheTracker - уже реализованный ICacheTagTracker, который работает на базе механизма CONTENT_MODIFICATION из QP
+    trackers.Register<QpContentCacheTracker>();
+});
 app.UseRouting();
 app.UseAuthorization();
 app.UseSwaggerUI();
