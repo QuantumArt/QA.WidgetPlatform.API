28b7ca8a61778b7f1d43a9b77929c2e20c42bfb9
Pavel Celut <CelutP@gmail.com>
2022-12-30 12:59:30 +0300

    Add CORS

--- QA.WidgetPlatform.Api/Program.cs
+++ QA.WidgetPlatform.Api/Program.cs
@@ -2,12 +2,24 @@ using QA.WidgetPlatform.Api.Application.Middleware;
 using QA.WidgetPlatform.Api.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.ConfigureBaseServicesWithoutInvalidation(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policyBuilder =>
+    {
+        policyBuilder.WithOrigins(
+            builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ??
+            Array.Empty<string>());
+    });
+});
+
+builder.Services.ConfigureBaseServices(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseMiddleware<StatusCodeExceptionHandlerMiddleware>();
 app.UseRouting();
+app.UseCors();
 app.UseAuthorization();
 app.UseSwaggerUI();
 app.MapControllers();
